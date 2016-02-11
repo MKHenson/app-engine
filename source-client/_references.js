b975b0ed63ad2c55963abe7321cb3fd1d78688fe
unknown <mat@webinate.net>
2016-02-11 17:17:31 +0000

    Removed all js files

--- source-client/_references.js
+++
@@ -1,9 +0,0 @@
-/// <reference path="./definitions/JQuery.d.ts" />
-/// <reference path="./definitions/JSColor.d.ts" />
-/// <reference path="./definitions/AceEditor.d.ts" />
-/// <reference path="./definitions/FileUploader.d.ts" />
-/// <reference path="./definitions/Recaptcha.d.ts" />
-/// <reference path="./definitions/ExportToken.d.ts" />
-/// <reference path="../source-server/custom-definitions/app-engine.d.ts" />
-define(["require", "exports"], function (require, exports) {
-});
--- source-client/lib/core/IDockItem.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=IDockItem.js.map
