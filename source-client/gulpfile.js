5301e9dad4bf2c263702273584869ae2342f2359
Mathew Henson <mat@webinate.net>
2016-02-25 21:35:52 +0000

    Clean up

--- source-client/gulpfile.js
+++ source-client/gulpfile.js
@@ -14,20 +14,11 @@ var gunzip = require('gulp-gunzip');
 var request = require('request');
 var source = require('vinyl-source-stream')
 var untar = require('gulp-untar');
-var mv = require('mv');
+var gutil = require('gulp-util');
 
 // Read the contents of the tsconfig file so we dont have to specify the files twice
 var tsConfig = JSON.parse(fs.readFileSync('tsconfig.json'));
 var tsFiles = tsConfig.files;
-
-// Make sure the files exist
-for (var i = 0, l = tsFiles.length; i < l; i++ )
-    if(!fs.existsSync(tsFiles[i]))
-    {
-        console.log("File does not exist:" + tsFiles[i] );
-        process.exit();
-    }
-
 
 // CONFIG
 // ==============================
@@ -41,31 +32,29 @@ var target = gulp.src('./lib/index.html');
 gulp.task('bower', function() {
 
     var sources = gulp.src([
-        './bower_components/jquery/dist/jquery.js',
-        './bower_components/jquery.hotkeys/jquery.hotkeys.js',
-        './bower_components/jquery-mousewheel/jquery.mousewheel.js',
-        './bower_components/jstepper/jquery.jstepper.js',
-        './bower_components/jquery.scrollTo/jquery.scrollTo.js',
-        './bower_components/jquery-ui/ui/jquery.ui.core.js',
-        './bower_components/jquery-ui/ui/jquery.ui.widget.js',
-        './bower_components/jquery-ui/ui/jquery.ui.mouse.js',
-        './bower_components/jquery-ui/ui/jquery.ui.draggable.js',
-        './bower_components/jquery-ui/ui/jquery.ui.droppable.js',
-        './bower_components/jquery-ui/ui/jquery.ui.resizable.js',
-        './bower_components/es6-promise/es6-promise.js',
-        './bower_components/google-recaptcha-api/index.js',
-        './bower_components/jscolor/*.*',
-        './bower_components/ace/src-noconflict/*.js',
-        './bower_components/ace/src-noconflict/snippets/javascript.js',
-        './bower_components/ace/src-noconflict/snippets/typescript.js',
-        './bower_components/ace/src-noconflict/snippets/html.js',
-        './bower_components/ace/src-noconflict/snippets/json.js',
-        './bower_components/google-recptcha-api/index.js'
+        './third-party/jquery/dist/jquery.js',
+        './third-party/jquery-hotkeys/jquery.hotkeys.js',
+        './third-party/jquery-mousewheel/jquery.mousewheel.js',
+        './third-party/jstepper/jquery.jstepper.js',
+        './third-party/jquery-scrollTo/jquery.scrollTo.js',
+        './third-party/jquery-ui/ui/core.js',
+        './third-party/jquery-ui/ui/widget.js',
+        './third-party/jquery-ui/ui/mouse.js',
+        './third-party/jquery-ui/ui/draggable.js',
+        './third-party/jquery-ui/ui/droppable.js',
+        './third-party/jquery-ui/ui/resizable.js',
+        './third-party/es6-promise/dist/es6-promise.js',
+        './third-party/jscolor/*.*',
+        './third-party/ace/src-noconflict/*.js',
+        './third-party/ace/src-noconflict/snippets/javascript.js',
+        './third-party/ace/src-noconflict/snippets/typescript.js',
+        './third-party/ace/src-noconflict/snippets/html.js',
+        './third-party/ace/src-noconflict/snippets/json.js'
     ], { base: "." } )
         .pipe(gulp.dest(outDir));
 
-    var sourceNoAce = sources.pipe( filter( ['**/*.js', '!bower_components/ace/**/*.js' ] ) );
-    var sourceWithAce = gulp.src(['bower_components/ace/src-noconflict/ace.js'], { base: "." } )
+    var sourceNoAce = sources.pipe( filter( ['**/*.js', '!third-party/ace/**/*.js' ] ) );
+    var sourceWithAce = gulp.src(['third-party/ace/src-noconflict/ace.js'], { base: "." } )
         .pipe( gulp.dest( outDir ) );
     var mergedStream = merge(sourceNoAce, sourceWithAce);
 
@@ -114,6 +103,20 @@ gulp.task('css', function() {
          }))
         .pipe(gulp.dest(outDir));
 });
+
+/**
+ * Checks to see that all TS files listed exist
+ */
+gulp.task('check-files', function(){
+
+    // Make sure the files exist
+    for (var i = 0, l = tsFiles.length; i < l; i++ )
+        if(!fs.existsSync(tsFiles[i]))
+        {
+            console.log("File does not exist:" + tsFiles[i] );
+            process.exit();
+        }
+})
 
 // Concatenates and builds all TS code into a single file
 gulp.task('ts-code', function() {
@@ -185,41 +188,83 @@ gulp.task('watch', function () {
     gulp.watch('lib/**/*.ts', ['ts-code']);
 });
 
+function deleteFolderRecursive(path) {
+    if( fs.existsSync(path) ) {
+        fs.readdirSync(path).forEach(function(file,index){
+            var curPath = path + "/" + file;
+            if(fs.lstatSync(curPath).isDirectory()) { // recurse
+                deleteFolderRecursive(curPath);
+            }
+            else
+                fs.unlinkSync(curPath);
+        });
+        fs.rmdirSync(path);
+    }
+};
+
+/**
+ * Downloads a tarbal from a given url and unzips it into a specified folder
+ * @param {string} url The URL of the tarball to download
+ * @param {string} folder The folder we are moving the contents to
+ */
 function downloadClient(url, folder){
-    return request(url)
+    return new Promise(function(resolve, reject){
+        gutil.log('Downloading file "'+ url +'" into folder "' + folder + '"');
+        return request(url)
         .pipe(source('hello.tar.gz'))
+        .on('end', function(){
+            gutil.log('Unzipping... "'+ url +'"');
+        })
         .pipe(gunzip())
         .pipe(untar())
-        //.pipe(filter( ['**/*.*']))
-        //.pipe(rebase('1^'))
-        //.pipe(flatten({ includeParents: [1, 1]} ))
         .pipe(gulp.dest(folder))
         .on('end', function() {
-            var folders = fs.readdirSync(folder)
-            var fs.readdirSync(folder)
-            mv(folder + '/' + folders[0], folder, {mkdirp: true}, function(err) {
-                // done. it first created all the necessary directories, and then
-                // tried fs.rename, then falls back to using ncp to copy the dir
-                // to dest and then rimraf to remove the source dir
+            var folders = fs.readdirSync(folder);
+            gulp.src( folder + '/' + folders[0] + "/**/*.*" )
+                .pipe(gulp.dest(folder))
+                .on('end', function() {
+                    deleteFolderRecursive(folder + '/' + folders[0]);
+                    gutil.log(gutil.colors.green('Finished download of "'+ url +'"'));
+                    resolve(true);
                 });
         })
+    });
 }
+
+/**
+ * Downloads each of the third party archives and unzips them into the third-party folder respectively
+ */
+gulp.task('install-third-parties', function () {
+    return Promise.all([
+        downloadClient("https://github.com/jquery/jquery/tarball/2.2.1", './third-party/jquery'),
+        downloadClient("https://github.com/jeresig/jquery.hotkeys/tarball/0.2.0", './third-party/jquery-hotkeys'),
+        downloadClient("https://github.com/EastDesire/jscolor/tarball/v1.4.5", './third-party/jscolor'),
+        downloadClient("https://github.com/EmKayDK/jstepper/tarball/1.5.0", './third-party/jstepper'),
+        downloadClient("https://github.com/ajaxorg/ace-builds/tarball/v1.2.3", './third-party/ace'),
+        downloadClient("https://github.com/jquery/jquery-ui/tarball/1.11.4", './third-party/jquery-ui'),
+        downloadClient("https://github.com/stefanpenner/es6-promise/tarball/v3.1.2", './third-party/es6-promise'),
+        downloadClient("https://github.com/jquery/jquery-mousewheel/tarball/3.1.13", './third-party/jquery-mousewheel'),
+        downloadClient("https://github.com/flesler/jquery.scrollTo/tarball/2.1.2", './third-party/jquery-scrollTo')
+    ]);
+});
+
+/**
+ * Downloads the definition files used in the development of the application and moves them into the definitions folder
+ */
+gulp.task('install-definitions', function () {
+     return merge(
+         download("https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/master/jquery/jquery.d.ts"),
+         download("https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/master/jqueryui/jqueryui.d.ts"),
+         download("https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/master/ace/ace.d.ts"),
+         download("https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/master/es6-promise/es6-promise.d.ts"),
+         download("https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/master/jquery.scrollTo/jquery.scrollTo.d.ts"),
+         download("https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/master/grecaptcha/grecaptcha.d.ts"))
+             .pipe(gulp.dest("lib/definitions/required"));
+});
 
 /**
  * Use this task to install all third-party libraries from github and their respective authors
  */
-gulp.task('install-definitions', function () {
+gulp.task('install', ['install-third-parties', 'install-definitions']);
 
-    downloadClient("https://github.com/ajaxorg/ace-builds/tarball/v1.2.3", './third-party/ace')
-
-    // merge(
-    //     download("https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/master/jquery/jquery.d.ts"),
-    //     download("https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/master/jqueryui/jqueryui.d.ts"),
-    //     download("https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/master/ace/ace.d.ts"),
-    //     download("https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/master/es6-promise/es6-promise.d.ts"),
-    //     download("https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/master/jquery.scrollTo/jquery.scrollTo.d.ts"),
-    //     download("https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/master/grecaptcha/grecaptcha.d.ts"))
-    //         .pipe(gulp.dest("definitions/"));
-});
-
-gulp.task('build-all', ['html', 'media', 'ts-code', 'ts-code-declaration', 'bower','css']);
+gulp.task('build-all', ['html', 'media', 'check-files', 'ts-code', 'ts-code-declaration', 'bower','css']);
