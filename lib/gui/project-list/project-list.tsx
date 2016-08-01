013c6fcad13dca852003c3960f4a1d81cc6535e0
Mathew Henson <mat@webinate.net>
2016-08-01 23:21:45 +0100

    Added a project list component. Related to #36

--- lib/gui/project-list/project-list.tsx
+++ lib/gui/project-list/project-list.tsx
@@ -1,10 +1,147 @@
 module Animate {
-    /**
-     * Project List
-     */
-    export class ProjectList {
-        constructor(props) {
 
+    /**
+     *  Extends the project with a selected attribute
+     */
+    export interface IInteractiveProject extends Engine.IProject {
+        selected?: boolean;
+    }
+
+    export interface IProjectListProps extends React.HTMLAttributes {
+        onProjectSelected?: (project : IInteractiveProject) => void;
+        noProjectMessage?: string;
+    }
+
+    export interface IProjectListState {
+        loading? : boolean;
+        selectedProject?: IInteractiveProject;
+        errorMsg? : string;
+        projects? : IInteractiveProject[];
+    }
+
+    /**
+     * A list that displays projects. Listen for the onProjectSelected event
+     * to react to project selections.
+     */
+    export class ProjectList extends React.Component<IProjectListProps, IProjectListState> {
+        static defaultProps : IProjectListProps = {
+            noProjectMessage: "You have no projects"
+        }
+
+        private $user: User;
+
+        /**
+         * Creates a new instance
+         */
+        constructor(props) {
+            super(props);
+            this.$user = User.get;
+            this.state = {
+                loading: false,
+                selectedProject: null,
+                errorMsg: null,
+                projects: []
+            };
+        }
+
+        /*
+         * Called when we select a project
+         * @param {IInteractiveProject} project The project to select
+         */
+        selectProject(project: IInteractiveProject) {
+            if ( this.state.selectedProject )
+                this.state.selectedProject.selected = false;
+
+            project.selected = true;
+            var selectedProject : IInteractiveProject;
+
+            if ( this.state.selectedProject != project ) {
+                selectedProject = project;
+                this.setState({ selectedProject : selectedProject });
+            }
+            else {
+                selectedProject = null;
+                this.state.selectedProject.selected = false;
+                this.setState({ selectedProject : selectedProject });
+            }
+
+            if ( this.props.onProjectSelected )
+                this.props.onProjectSelected(selectedProject);
+        }
+
+        /*
+        * Fetches a list of user projects
+        * @param {number} index
+        * @param {number} limit
+        */
+        fetchProjects(index: number, limit: number) : Promise<number> {
+            this.setState({
+                loading: true,
+                errorMsg: null,
+                selectedProject: null
+            });
+
+            return new Promise<number>( ( resolve, reject ) => {
+                this.$user.getProjectList( index, limit ).then( (projects) => {
+                    this.setState({
+                        loading: false,
+                        projects: projects.data
+                    });
+
+                    resolve(projects.count || 1);
+                }).catch( (err: Error) => {
+                    this.setState({
+                        loading: false,
+                        errorMsg: err.message
+                    });
+                    reject(err);
+                });
+            });
+        }
+
+        /**
+         * Creates the component elements
+         * @returns {JSX.Element}
+         */
+        render(): JSX.Element {
+            const props : IProjectListProps  = Object.assign({}, this.props);
+            delete props.noProjectMessage;
+            delete props.onProjectSelected;
+
+            return <div {...props} className={'project-list ' + (props.className || '')}>
+                <div className="projects-toolbar background">
+                    {this.props.children}
+                    <SearchBox placeholder="Keywords" />
+                </div>
+                <div className="projects-container">
+                    <Pager onUpdate={this.fetchProjects.bind(this)} limit={2}>
+                        <div className="project-items">
+                            <div className="error bad-input" style={{ display: (this.state.errorMsg ? 'block' : '' ) }}>
+                                {this.state.errorMsg || ''}
+                            </div>
+                            {this.state.projects.map(( p, index )=>{
+                                return <div
+                                    key={p._id}
+                                    className="project-item img-preview unselectable"
+                                    onClick={()=>{
+                                            this.selectProject(p)
+                                        }}>
+                                        <div className="preview-child">
+                                            <div className="background-tiles inner ng-scope">
+                                                <img className="vert-align" src={( p.image && p.image != '' ? p.image : './media/appling.png' )} />
+                                                <div className="div-center"></div>
+                                            </div>
+                                        </div>
+                                        <div className={ 'item-name' + (p.selected ? ' reg-gradient' : '') }><span className="fa fa-file"/>{p.name}</div>
+                                    </div>
+                            })}
+                            <div className="no-items unselectable" style={{ display: (this.state.projects.length ? 'none' : '')}}>
+                                {this.props.noProjectMessage}
+                            </div>
+                        </div>
+                    </Pager>
+                </div>
+            </div>
         }
     }
 }
