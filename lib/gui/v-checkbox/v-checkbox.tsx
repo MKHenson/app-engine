158df51c9048050418c8ac7294e7111061224f37
Mathew Henson <mat@webinate.net>
2016-08-03 15:08:15 +0100

    Several bug fixes to the VCheckbox

--- lib/gui/v-checkbox/v-checkbox.tsx
+++ lib/gui/v-checkbox/v-checkbox.tsx
@@ -1,9 +1,14 @@
 module Animate {
-    export class VCheckbox extends React.Component<React.HTMLAttributes, {checked?: boolean, className?: string; pristine? : boolean; }> {
+
+    export interface IVCheckboxProps extends React.HTMLAttributes {
+        onChecked?: (e : React.FormEvent, checked : boolean ) => void;
+    }
+
+    export class VCheckbox extends React.Component<IVCheckboxProps, {checked?: boolean, className?: string; pristine? : boolean; }> {
         /**
          * Creates an instance
          */
-        constructor(props: React.HTMLAttributes) {
+        constructor(props: IVCheckboxProps) {
             super(props);
             this.state = {
                 checked : props.checked || false,
@@ -22,7 +27,16 @@ module Animate {
                 pristine: false
             } );
             if (this.props.onChange)
-                this.props.onChange(e)
+                this.props.onChange(e);
+            if (this.props.onChecked)
+                this.props.onChecked(e, (e.target as HTMLInputElement).checked);
+        }
+
+        componentWillReceiveProps(nextProps: IVCheckboxProps) {
+            this.setState({
+                className : ( "v-checkbox fa " + ( nextProps.className || '' ) ),
+                checked: ( nextProps.checked !== undefined ? nextProps.checked : this.state.checked )
+            });
         }
 
         /**
@@ -38,11 +52,12 @@ module Animate {
          * @returns {JSX.Element}
          */
         render(): JSX.Element {
-            let props : IVInputProps  = Object.assign({}, this.props);
+            let props : IVCheckboxProps  = Object.assign({}, this.props);
             delete props.id;
             delete props.name;
             delete props.checked;
             delete props.label;
+            delete props.onChecked;
 
             var className = this.state.className;
             if (!this.state.pristine)
@@ -58,9 +73,13 @@ module Animate {
                     checked={this.state.checked} value={this.state.checked} />
                 <label
                     onClick={(e)=>{
-                        this.setState({ checked: !this.state.checked });
-                        if (this.props.onChange)
-                            this.props.onChange(e);
+                        if ( !this.props.id ) {
+                            this.setState({ checked: !this.state.checked });
+                            if ( this.props.onChange)
+                                this.props.onChange(e);
+                            if (this.props.onChecked)
+                                this.props.onChecked(e, (e.target as HTMLInputElement).checked);
+                        }
                     }}
                     className="unselectable"
                     htmlFor={(this.props.id ? this.props.id : null)}>
