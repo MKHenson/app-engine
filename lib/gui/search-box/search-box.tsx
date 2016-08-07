b5742c9cf55238c27679b5ff4ee6121de4456cfb
Mathew Henson <mat@webinate.net>
2016-08-07 16:52:36 +0100

    Changed onChange to onSearch and maintained value in state

--- lib/gui/search-box/search-box.tsx
+++ lib/gui/search-box/search-box.tsx
@@ -1,17 +1,30 @@
 module Animate {
+    export interface ISearchBoxProps extends React.HTMLAttributes {
+        onSearch(e: React.FormEvent, searchText: string);
+    }
 
     /**
      * Wraps an input box with HTML that makes it look like a search bar.
      * Add a listener for the onChange event and it will be triggered either when the input
      * changes, or the search button is pressed.
      */
-    export class SearchBox extends React.Component<React.HTMLAttributes, any> {
+    export class SearchBox extends React.Component<ISearchBoxProps, { value: string }> {
 
         /**
          * Creates an instance of the search box
          */
-        constructor(props: React.HTMLAttributes) {
+        constructor(props: ISearchBoxProps) {
             super(props);
+            this.state = { value : props.value as string };
+        }
+
+        /**
+         * Called when the props are updated
+         */
+        componentWillReceiveProps(nextProps: IVCheckboxProps) {
+            this.setState({
+                value: ( nextProps.value as string !== undefined ? nextProps.value as string : this.state.value )
+            });
         }
 
         /**
@@ -19,16 +32,23 @@ module Animate {
          * @returns {JSX.Element}
          */
         render(): JSX.Element {
-            let props : IVFormProps  = Object.assign({}, this.props);
+            let props : ISearchBoxProps  = Object.assign({}, this.props);
+            let text = this.state.value;
             delete props.id;
             delete props.name;
             delete props.placeholder;
+            delete props.onSearch;
 
             return <div {...props} className={'search-box ' + (props.className ? props.className : '' )}>
                 <input
-                    onChange={(e) =>{
-                        if (this.props.onChange)
-                            this.props.onChange(e);
+                    onChange={(e) => {
+                        let text = (e.target as HTMLInputElement).value as string ;
+                        this.setState({
+                            value : text
+                        });
+
+                        if (this.props.onSearch)
+                            this.props.onSearch(e, text);
                     }}
                     type="text"
                     id={this.props.id}
@@ -37,11 +57,8 @@ module Animate {
                 />
                 <label
                     onClick={(e)=>{
-                        if ( this.props.id )
-                            return;
-
-                        if (this.props.onChange)
-                            this.props.onChange(e);
+                        if (this.props.onSearch)
+                            this.props.onSearch(e, this.state.value);
                     }}
                     htmlFor={this.props.id || undefined}><span className="fa fa-search" /></label>
             </div>
