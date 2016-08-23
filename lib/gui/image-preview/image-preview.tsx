2aac3c04ae731f23455100d16676adcedb0a2228
Mathew Henson <mat@webinate.net>
2016-08-23 10:17:24 +0100

    ImagePreview now has a preloader

--- lib/gui/image-preview/image-preview.tsx
+++ lib/gui/image-preview/image-preview.tsx
@@ -8,25 +8,54 @@ module Animate {
         className? : string;
         selected? : boolean;
         onLabelClick? : (e: React.MouseEvent) => void;
+        showLoadingIcon? : boolean;
     }
 
     /**
      * Shows an image in a against transparent backdrop that is vertically centered and scaled into its container
      */
-    export class ImagePreview extends React.Component<IImagePreviewProps, any> {
+    export class ImagePreview extends React.Component<IImagePreviewProps, { loading : boolean }> {
 
         static defaultProps: IImagePreviewProps = {
             selected: false,
             src: null,
             label: null,
-            defaultSrc: './media/appling.png'
+            defaultSrc: './media/appling.png',
+            showLoadingIcon: false
         }
+
+        private _imgLoader: HTMLImageElement;
 
         /**
          * Creates an instance
          */
         constructor(props: IImagePreviewProps) {
-            super(props)
+            super(props);
+            this.state = { loading : false };
+            this._imgLoader = new Image();
+            this._imgLoader.onload = () => {
+                this.setState({ loading : false });
+            }
+        }
+
+        /**
+         * When the preview is added we start the loading process
+         */
+        componentDidMount() {
+            this._imgLoader.src = this.props.src || this.props.defaultSrc;
+            this.setState({ loading : true });
+        }
+
+        /**
+         * If the src or default props change, we reload the new image
+         */
+        componentWillReceiveProps(nextProps : IImagePreviewProps) {
+            let nextSrc = nextProps.src || nextProps.defaultSrc;
+            let curSrc = this.props.src || this.props.defaultSrc;
+            if ( nextSrc != curSrc ) {
+                this._imgLoader.src = nextProps.src;
+                this.setState({ loading : true });
+            }
         }
 
         /**
@@ -40,13 +69,21 @@ module Animate {
             delete props.defaultSrc;
             delete props.labelIcon;
             delete props.onLabelClick;
+            delete props.showLoadingIcon;
+
+            let img = <i className="fa fa-cog fa-spin fa-3x fa-fw"></i>;
+
+            if (!this.props.showLoadingIcon) {
+                if (!this.state.loading)
+                    img = <img ref="img" className="vert-align" src={this._imgLoader.src}/>
+            }
 
             let className = 'image-preview unselectable' + ( this.props.className ? ' ' + this.props.className : '' );
 
             return <div {...props} className={className}>
                 <div className="preview-child">
                     <div className="background-tiles inner">
-                        <img className="vert-align" src={this.props.src || this.props.defaultSrc }/>
+                        {img}
                         <div className="div-center"></div>
                     </div>
                 </div>
