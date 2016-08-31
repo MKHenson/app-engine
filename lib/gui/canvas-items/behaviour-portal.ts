3859ea11a217007c7fa2db1fbf00d82ae6331289
Mathew Henson <mat@webinate.net>
2016-08-31 17:02:14 +0100

    Added behaviour portal TSX

--- lib/gui/canvas-items/behaviour-portal.ts
+++ lib/gui/canvas-items/behaviour-portal.ts
@@ -1,68 +1,69 @@
-// module Animate {
-// 	export class BehaviourPortal extends Behaviour {
-// 		private _portalType: PortalType;
-//         private _property: Prop<any>;
+module Animate {
 
-//         constructor(parent: Component, property: Prop<any>, portalType: PortalType = PortalType.INPUT) {
-// 			// Call super-class constructor
-//             super(parent, (property ? property.name : "Portal"));
+    /**
+     * A behaviour for representing container portals
+     */
+	export class BehaviourPortal extends Behaviour {
+		private _portalType: PortalType;
+        private _property: Prop<any>;
 
-//             this._portalType = portalType;
-//             this._property = property;
+        /**
+         * Creates an instance
+         */
+        constructor( property: Prop<any>, portalType: PortalType = 'input' ) {
+            super();
 
-//             this.element.addClass("behaviour-portal");
+            this.alias = property.name;
+            this.behaviourType = 'portal';
+            this._portalType = portalType;
+            this._property = property;
+            this.className = 'behaviour-portal ' + portalType;
 
+            if (property) {
+                if (this._portalType == 'output')
+                    this.addPortal('input', property, true);
+                else if (this._portalType == 'input')
+                    this.addPortal('output', property, true);
+                else if (this._portalType == 'parameter')
+                    this.addPortal('product', property, true);
+                else if (this._portalType == 'product')
+                    this.addPortal('parameter', property, true);
+            }
+        }
 
-//             if (property) {
-//                 if (this._portalType == PortalType.OUTPUT)
-//                     this.addPortal(PortalType.INPUT, property, true);
-//                 else if (this._portalType == PortalType.INPUT)
-//                     this.addPortal(PortalType.OUTPUT, property, true);
-//                 else if (this._portalType == PortalType.PARAMETER)
-//                     this.addPortal(PortalType.PRODUCT, property, true);
-//                 else if (this._portalType == PortalType.PRODUCT)
-//                     this.addPortal(PortalType.PARAMETER, property, true);
+        /**
+         * Serializes the data into a JSON.
+		 * @param {number} id
+         * @returns {IBehaviourPortal}
+         */
+        serialize(id: number): IBehaviourPortal {
+            var toRet = <IBehaviourPortal>super.serialize(id);
+            toRet.portal = { name: this._property.name, custom: true, type: this._portalType, property: this._property.tokenize() };
+            toRet.type = 'portal';
+            return toRet;
+        }
 
-//                 this.element.addClass(PortalType[this._portalType].toLowerCase());
-//             }
-//         }
+        /**
+         * De-Serializes data from a JSON.
+         * @param {IBehaviourPortal} data The data to import from
+         */
+        deSerialize(data: IBehaviourPortal) {
+            super.deSerialize(data);
+            this._portalType = data.portal.type;
+            this.className = 'behaviour-portal ' + this._portalType;
+            this._property = Utils.createProperty(data.portal.property.name, data.portal.property.type);
+            this._property.deTokenize(data);
+        }
 
-//         /**
-//         * Tokenizes the data into a JSON.
-//         * @param {boolean} slim If true, only the core value is exported. If false, additional data is exported so that it can be re-created at a later stage
-//         * @returns {IBehaviourPortal}
-//         */
-//         tokenize(slim: boolean = false): IBehaviourPortal {
-//             var toRet = <IBehaviourPortal>super.tokenize(slim);
-//             toRet.portal = { name: this._property.name, custom: true, type: this._portalType, property: this._property.tokenize(slim) };
-//             toRet.type = CanvasItemType.BehaviourPortal;
-//             return toRet;
-//         }
+		/**
+         * This will cleanup the component.
+         */
+		dispose() {
+            this._property = null;
+			super.dispose();
+		}
 
-//         /**
-//         * De-Tokenizes data from a JSON.
-//         * @param {IBehaviourPortal} data The data to import from
-//         */
-//         deTokenize(data: IBehaviourPortal) {
-//             super.deTokenize(data);
-//             this._portalType = data.portal.type;
-//             this.element.addClass(PortalType[this._portalType].toLowerCase());
-//             this._property = Utils.createProperty(data.portal.property.name, data.portal.property.type);
-//             this._property.deTokenize(data);
-//         }
-
-// 		/**
-//         * This will cleanup the component.
-//         */
-// 		dispose() {
-// 			this._portalType = null;
-//             this._property = null;
-
-// 			// Call super
-// 			super.dispose();
-// 		}
-
-// 		get portaltype(): PortalType { return this._portalType; }
-//         get property(): Prop<any> { return this._property; }
-// 	}
-// }
+		get portaltype(): PortalType { return this._portalType; }
+        get property(): Prop<any> { return this._property; }
+	}
+}
