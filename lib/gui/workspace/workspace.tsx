6653f1cac35c971a992ad82c11c5b2fc2a02da20
Mathew Henson <mat@webinate.net>
2016-09-21 17:16:48 +0100

    Only listening for change events now

--- lib/gui/workspace/workspace.tsx
+++ lib/gui/workspace/workspace.tsx
@@ -2,13 +2,13 @@ namespace Animate {
 
     export interface IWorkspaceProps extends ITabProps {
         project: Project;
-        onSchemaActivated: ( schema: ContainerSchema ) => void;
     }
 
 	/**
 	 * The main workspace area of the application.
 	 */
     export class Workspace extends React.Component<IWorkspaceProps, any> {
+        private _previousEditor: Editor;
 
         /**
          * Creates an instance of the workspace
@@ -17,31 +17,30 @@ namespace Animate {
             super( props );
         }
 
+        /**
+         * Bind any project related events
+         */
         componentWillMount() {
-
-            // add project events
-            this.props.project.on<ProjectEvents, IEditorEvent>( 'editor-created', this.onSchemaCreated, this );
-            this.props.project.on<ProjectEvents, IEditorEvent>( 'editor-removed', this.onSchemaRemoved, this );
+            this.props.project.on<ProjectEvents, void>( 'change', this.onProjectChanged, this );
         }
 
+        /**
+         * Unbind any project related events
+         */
         componentWillUnmount() {
-            // remove project events
-            this.props.project.off<ProjectEvents, IEditorEvent>( 'editor-created', this.onSchemaCreated, this );
-            this.props.project.off<ProjectEvents, IEditorEvent>( 'editor-removed', this.onSchemaRemoved, this );
+            this.props.project.off<ProjectEvents, void>( 'change', this.onProjectChanged, this );
         }
 
-        onSchemaRemoved( type: ProjectEvents, event: IEditorEvent ) {
-            if ( this.props.project.openEditors.length == 0 )
-                this.props.onSchemaActivated( null );
-        }
-
-        onSchemaCreated( type: ProjectEvents, event: IEditorEvent ) {
+        /**
+         * Update the workspace
+         */
+        onProjectChanged( type: ProjectEvents ) {
             this.forceUpdate();
         }
 
         canContainerClose( editor: Editor ): boolean | Promise<boolean> {
 
-            if ( !editor.resource.saved ) {
+            if ( !editor.resource.saved || editor.hasUndos ) {
                 return new Promise<Boolean>( function ( resolve, reject ) {
                     MessageBox.warn(
                         `'${editor.resource.entry.name}' is not saved. Do you want to save it before closing?`,
@@ -77,20 +76,21 @@ namespace Animate {
                             <h2>Welcome back {User.get.entry.username}</h2>
                             <ButtonLink>Create Container</ButtonLink>
                             <ButtonLink>Open the Docs</ButtonLink>
-
                         </div>
                     ) : (
                             <Tab
                                 panes={ editors.map(( editor, index ) => {
                                     return (
                                         <TabPane
-                                            onSelect={ ( index ) => { this.props.onSchemaActivated( editor as ContainerSchema ) } }
+                                            onSelect={ ( index ) => {
+                                                this.props.project.activateEditor( editor );
+                                            } }
                                             key={'pane-' + index}
                                             canClose={ ( i, props ) => {
                                                 return this.canContainerClose( editor );
                                             } }
 
-                                            label={ ( editor.resource.saved ? '' : '* ' ) + editor.resource.entry.name}>
+                                            label={ ( !editor.resource.saved || editor.hasUndos ? '* ' : '' ) + editor.resource.entry.name}>
                                             <Schema editor={editor as ContainerSchema} />
                                         </TabPane>
                                     )
