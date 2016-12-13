29b026cdb9027861b7f11bdc0b9ecc3c6953b696
Mathew Henson <mat@webinate.net>
2016-12-13 17:10:39 +0000

    Further fleshed out the router functionality

--- lib/core/router.ts
+++ lib/core/router.ts
@@ -2,9 +2,17 @@ import { EventDispatcher } from './event-dispatcher';
 
 export interface IState {
     name: string;
-    title?: string;
     path: string;
     queries: any;
+    params: any;
+}
+
+export interface IRoute {
+    name: string;
+    path: string;
+    title?: string;
+    isIndex?: boolean;
+    onStateEnter: ( route: IState ) => void;
 }
 
 /**
@@ -12,42 +20,145 @@ export interface IState {
  */
 export class Router extends EventDispatcher {
     private static _singleton: Router;
+    private mode: 'history' | 'hash';
+    private root: string;
+    private _routes: IRoute[];
 
     /**
      * Creates a state manager
      */
     constructor() {
         super();
+        this.mode = 'history';
+        this.root = '/';
     }
 
-    init() {
+    /**
+     * Initializes the router and its routes
+     */
+    init( routes: IRoute[] ) {
+        this._routes = routes;
 
         window.addEventListener( 'popstate', this.onPopState.bind( this ) );
+        this.check();
+    }
 
-        const path = window.location.pathname;
+    /**
+     * Triggers the events when a route path has been matched
+     */
+    triggerStateChange( route: IRoute, path: string ) {
+        const queries = this.getQueryParams();
+        const params = {};
+        const routeParts = route.path.match( /([^\/]+)/g );
+        const pathParts = path.match( /([^\/]+)/g );
 
+        if ( routeParts ) {
+            for ( let i = 0, l = routeParts.length; i < l; i++ ) {
+                if ( routeParts![ i ][ 0 ] === ':' )
+                    params[ routeParts![ i ].replace( ':', '' ) ] = pathParts![ i ];
+            }
+        }
 
-
-        this.push( {
-            name: '',
+        route.onStateEnter( {
+            name: route.name,
+            params: params,
             path: path,
-            queries: this.getQueryParams()
+            queries: queries
         });
+    }
+
+    /**
+     * Checks the path and triggers any state changes if they match the path
+     */
+    check( pathToCheck?: string ) {
+        const path: string = pathToCheck || this.getPath();
+        const pathParts = path.match( /([^\/]+)/g );
+
+        if ( pathParts ) {
+
+            const numPathParts = pathParts.length;
+
+            for ( const route of this._routes ) {
+
+                if ( route.path === '*' ) {
+                    this.triggerStateChange( route, path );
+                    return route;
+                }
+
+                const routeParts = route.path.match( /([^\/]+)/g );
+
+                if ( routeParts ) {
+
+                    const numRouteParts = routeParts.length;
+                    let pathMatches = true;
+
+                    if ( numPathParts > numRouteParts )
+                        continue;
+
+                    for ( let i = 0; i < numRouteParts; i++ ) {
+
+                        let isVariable = routeParts[ i ][ 0 ] === ':' ? true : false;
+                        let isOptional = routeParts[ i ][ routeParts[ i ].length - 1 ] === '?' ? true : false;
+
+                        if ( !pathParts[ i ] && !isOptional ) {
+                            pathMatches = false;
+                            break;
+                        }
+
+
+                        if ( pathParts[ i ] !== routeParts[ i ] && !isVariable ) {
+                            pathMatches = false;
+                            break;
+                        }
+                    }
+
+                    if ( pathMatches ) {
+                        this.triggerStateChange( route, path );
+                        return route;
+                    }
+                }
+            }
+        }
+        else {
+            for ( const route of this._routes )
+                if ( route.isIndex ) {
+                    this.triggerStateChange( route, path );
+                    return route;
+                }
+        }
     }
 
     /**
      * Removes begining and trailing slashes
      */
-    clearSlashes( path : string ): string {
-        return path.toString().replace(/\/$/, '').replace(/^\//, '');
+    clearSlashes( path: string ): string {
+        return path.toString().replace( /\/$/, '' ).replace( /^\//, '' );
     }
 
     /**
-     * Returns the queries as an object
+     * A function that gets the path section of a url.
+     * The returned result is stripped of any trailing slashes.
+     */
+    getPath() {
+        let fragment = '';
+        if ( this.mode === 'history' ) {
+            fragment = this.clearSlashes( decodeURI( location.pathname + location.search ) );
+            fragment = fragment.replace( /\?(.*)$/, '' );
+            fragment = this.root != '/' ? fragment.replace( this.root, '' ) : fragment;
+
+        } else {
+            var match = window.location.href.match( /#(.*)$/ );
+            fragment = match ? match[ 1 ] : '';
+        }
+        return this.clearSlashes( fragment );
+    }
+
+    /**
+     * Returns the queries of a url as an object
      */
     getQueryParams() {
-        var match,
-            pl = /\+/g,  // Regex for replacing addition symbol with a space
+        let match;
+        const pl = /\+/g,  // Regex for replacing addition symbol with a space
             search = /([^&=]+)=?([^&]*)/g,
             query = window.location.search.substring( 1 ),
             decode = ( s ) => decodeURIComponent( s.replace( pl, " " ) );
@@ -60,20 +171,31 @@ export class Router extends EventDispatcher {
     }
 
     /**
-     * Called whenever the state pops
+     * Called whenever the state changes either by the user
+     * or from a manual call to push
      */
     protected onPopState( ev: PopStateEvent ) {
-        const state = ev.state as IState;
-
-        if ( state && state.name )
-            this.emit<string, IState>( state.name, ev.state as IState );
+        this.check( this.getPath() );
+        this.emit<string, IRoute>( 'onStateChange', ev.state as IRoute );
     }
 
     /**
-     * Add a history entry using push state
+     * Gets the history to go back a state
      */
-    push( data: IState ) {
-        history.pushState( data, data.title, data.path );
+    back() {
+        history.back();
+    }
+
+    /**
+     * Triggers a history route update
+     * @param path The path to change the url to
+     */
+    push( path: string ) {
+        let route = this.check( path );
+        if ( route )
+            history.pushState( undefined, route.title, path );
+        else
+            history.pushState( undefined, undefined, path );
     }
 
     /**
