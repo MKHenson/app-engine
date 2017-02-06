c8a1d46b792cc92a8068e09da6a0f74eba33143d
Mathew Henson <mat@webinate.net>
2017-02-06 23:24:22 +0000

    Completed Tab functions and comments

--- lib/components/tab/tab.ts
+++ lib/components/tab/tab.ts
@@ -1,35 +1,63 @@
-// import { ITabPaneProps } from './tab-pane';
 import { Menu, PopupMenuItem } from '../menu/menu';
 import { JML } from '../../jml/jml';
 
 /**
- * A specialized element that is added to Tab elements
+ * A specialized element that is added to Tab elements. You can sub class panes
+ * and create your own custom elements that can be added to Tab components.
+ *
+ * eg:
+ * const tab = new Tab();
+ * tab.addPane( new TabPane( 'New Pane' ) )
+ *     .innerHTML = 'This is the first tab';
  */
 export class TabPane extends HTMLElement {
 
     public label: string;
-    public showCloseButton: boolean;
+    public closeButton: boolean;
 
-    constructor( label: string = 'New Tab' ) {
+    constructor( label: string = 'New Tab', closeButton: boolean = true ) {
         super();
 
         this.label = label;
-        this.showCloseButton = true;
+        this.closeButton = closeButton;
     }
 
+    /**
+     * Overridable function to determine if we can select the tab pane
+     */
     async canSelect() {
         return true;
     }
 
+    /**
+     * Overridable function to determine if we can close the pane
+     */
     async canClose() {
         return true;
     }
 }
 
 /**
- * A Tab Component for organising pages of content into separate labelled tabs/components
+ * A Tab Component for organising an array of panes. Only one pane can be active at any time,
+ * and each can be activated by a label managed in the tab.
+ *
+ * eg:
+ * const tab = new Tab();
+ * tab.addPane( new TabPane( 'Tab 1' ) )
+ *     .innerHTML = 'This is the first tab';
+ *
+ * tab.addPane( new TabPane( 'Tab 2' ) )
+ *     .innerHTML = 'This is the second tab';
+ *
+ * document.body.appendChild( tab );
  */
 export class Tab extends HTMLElement {
+
+    static get observedAttributes() {
+        return [
+            'selected-index'
+        ];
+    }
 
     private _selectedIndex: number;
     private _panes: TabPane[];
@@ -55,11 +83,31 @@ export class Tab extends HTMLElement {
         super.appendChild( JML.div( { className: 'tab-panes' }) );
     }
 
+    /**
+     * If the attributes change we update the internal state
+     */
+    attributeChangedCallback( name: string, oldValue: string, newValue: string ) {
+        switch ( name ) {
+            case 'selected-index':
+                this.selectedIndex = parseInt( newValue );
+                break;
+        }
+    }
+
+    /**
+     * Gets the selected tab index
+     */
     get selectedIndex(): number {
         return this._selectedIndex;
     }
 
+    /**
+     * Sets the selected tab index
+     */
     set selectedIndex( val: number ) {
+        if ( val === this._selectedIndex )
+            return;
+
         const panels = this.querySelector( '.tab-panes' ) !;
         let canSelect: Promise<boolean>;
 
@@ -120,23 +168,46 @@ export class Tab extends HTMLElement {
         this.classList.toggle( 'has-panes', false );
     }
 
-    // /**
-    //  * Check if the index changes so we can notify the onSelect event
-    //  */
-    // attributeChangedCallback( attributeName: string, oldValue: string, newValue: string ) {
-    //     if (attributeName === 'selectedIndex' ) {
+    /**
+     * Adds a pane to the tab
+     */
+    addPane( pane: TabPane ): TabPane {
+        const labels = this.querySelector( '.tab-labels' ) !;
+        this._panes.push( pane );
 
-    //     }
-    //     if ( prevState.selectedIndex !== this.state.selectedIndex )
-    //         if ( this._panes[ this.state.selectedIndex ].props.onSelect )
-    //             this._panes[ this.state.selectedIndex ].props.onSelect!( this.state.selectedIndex );
-    // }
+        this.classList.toggle( 'has-panes', true );
+
+
+        const newLabel =
+            JML.div( {
+                className: 'tab-label', onclick: () => {
+                    const index = this._panes.indexOf( pane );
+                    this.selectedIndex = index;
+                }
+            }, [
+                    JML.div( { classList: 'text' }, [
+                        JML.span( { className: 'content' }, pane.label )
+                    ] ),
+                    JML.div( {
+                        classList: 'tab-close', onclick: ( e: MouseEvent ) => {
+                            e.stopPropagation();
+                            this.removePane( pane );
+                        }
+                    }, 'X' )
+                ] );
+
+        this._labels.push( newLabel );
+        labels.appendChild( newLabel );
+
+        this.selectedIndex = this._panes.length - 1;
+        return pane;
+    }
 
     /**
-     * Removes a pane from from the tab
+     * Removes a pane from the tab
      * @param pane The pane to remove
      */
-    private removePane( pane: TabPane ) {
+    removePane( pane: TabPane ) {
         const index = this._panes.indexOf( pane );
         let canClose: Promise<boolean> | boolean;
         if ( pane.canClose )
@@ -191,66 +262,36 @@ export class Tab extends HTMLElement {
      * Shows the context menu
      */
     showContext( e: MouseEvent ) {
+        e.preventDefault();
+        e.stopPropagation();
+
         let panes = this._panes;
         const menu = new Menu();
         for ( let pane of panes )
             menu.addItem( new PopupMenuItem( pane.label ) );
 
         menu.show( e.pageX, e.pageY );
-        menu.onItemClick = ( e, item ) => {
-            for ( let i = 0, l = panes.length; i < l; i++ )
-                if ( panes[ i ].label === item.text )
-                    return this.selectedIndex = i;
-        }
+        menu.onItemClick = ( e, item, index ) => this.selectedIndex = index;
     }
 
     /**
-     * Adds a pane to the tab
+     * Gets a tab's' props by either its label or index
+     * @param selector The label or index of the tab pane to return
      */
-    add( pane: TabPane ): TabPane {
-        const labels = this.querySelector( '.tab-labels' ) !;
-        this._panes.push( pane );
-
-        this.classList.toggle( 'has-panes', true );
-
-
-        const newLabel =
-            JML.div( {
-                className: 'tab-label', onclick: () => {
-                    const index = this._panes.indexOf( pane );
-                    this.selectedIndex = index;
-                }
-            }, [
-                    JML.div( { classList: 'text' }, [
-                        JML.span( { className: 'content' }, pane.label )
-                    ] ),
-                    JML.div( {
-                        classList: 'tab-close', onclick: ( e: MouseEvent ) => {
-                            e.stopPropagation();
-                            this.removePane( pane );
-                        }
-                    }, 'X' )
-                ] );
-
-        this._labels.push( newLabel );
-        labels.appendChild( newLabel );
-
-        this.selectedIndex = this._panes.length - 1;
-        return pane;
-    }
-
-    /**
-     * Gets a tab's' props by its label
-     * @param val The label text of the tab
-     */
-    getPaneByLabel( label: string ): TabPane | null {
+    get( selector: number | string ): TabPane | null {
         const panes = this._panes;
-        for ( const pane of panes )
-            if ( pane.label === label )
-                return pane;
+
+        if ( typeof ( selector ) === 'string' ) {
+            for ( const pane of panes )
+                if ( pane.label === selector )
+                    return pane;
+        }
+        else
+            return panes[ selector ] || null;
 
         return null;
     }
+
 
     /**
      * Gets an array of all the tab props
