ebbb1464b5f27423b1573309020cc6833e57ac49
Mathew Henson <mat@webinate.net>
2017-02-07 00:30:34 +0000

    Started implementing project list

--- lib/components/project-list/project-list.ts
+++ lib/components/project-list/project-list.ts
@@ -1,44 +1,88 @@
-import { Pager } from '../pager/pager';
-import { ImagePreview } from '../image-preview/image-preview';
+import { JML } from '../../jml/jml';
+// import { Pager } from '../pager/pager';
+// import { ImagePreview } from '../image-preview/image-preview';
 import { SearchBox } from '../search-box/search-box';
-
-export interface IProjectListProps extends React.HTMLAttributes {
-    onProjectSelected?: ( project: HatcheryServer.IProject ) => void;
-    onProjectDClicked?: ( project: HatcheryServer.IProject ) => void;
-    noProjectMessage?: string;
-    projects?: HatcheryServer.IProject[];
-    numProjects?: number;
-    onProjectsRequested?: ( index: number, limit: number, keywords: string ) => void;
-}
-
-export interface IProjectListState {
-    loading?: boolean;
-    searchText?: string;
-    selectedProject?: HatcheryServer.IProject | null;
-    errorMsg?: string | null;
-    projects?: HatcheryServer.IProject[];
-}
 
 /**
  * A list that displays projects in a paginated container.
  */
-export class ProjectList extends React.Component<IProjectListProps, IProjectListState> {
-    static defaultProps: IProjectListProps = {
-        noProjectMessage: 'You have no projects'
-    }
+export class ProjectList extends HTMLElement {
+
+    public onProjectSelected?: ( project: HatcheryServer.IProject ) => void;
+    public onProjectDClicked?: ( project: HatcheryServer.IProject ) => void;
+    public noProjectMessage?: string;
+    public projects?: HatcheryServer.IProject[];
+    public numProjects?: number;
+    public onProjectsRequested?: ( index: number, limit: number, keywords: string ) => void;
+
+    private _loading?: boolean;
+    private _searchText?: string;
+    private _selectedProject?: HatcheryServer.IProject | null;
+    private _errorMsg?: string | null;
+    private _projects?: HatcheryServer.IProject[];
 
     /**
      * Creates a new instance
      */
-    constructor( props ) {
-        super( props );
-        this.state = {
-            loading: false,
-            selectedProject: null,
-            errorMsg: null,
-            projects: [],
-            searchText: ''
-        };
+    constructor() {
+        super();
+        this.noProjectMessage = 'You have no projects';
+        this._loading = false;
+        this._selectedProject = null;
+        this._errorMsg = null;
+        this._projects = [];
+        this._searchText = '';
+
+        this.appendChild( JML.div( { className: 'projects-toolbar background' }, [
+            JML.elm<SearchBox>( new SearchBox(), {
+                placeholder: 'Keywords', onSearch: ( e, text ) => {
+                    this._searchText = text;
+                    // TODO: ( this.refs[ 'pager' ] as Pager ).invalidate();
+                }
+            }),
+            JML.i( { className: 'loader fa fa-cog fa-spin fa-3x fa-fw' })
+        ] ) );
+
+        this.appendChild( JML.div( { className: 'projects-container', onclick: ( e ) => this.selectProject( null, false ) }, [
+            'Pager Goes here'
+        ] ) );
+
+
+        //         <Pager
+        //             limit={6}
+        //             ref="pager"
+        //             count={this.props.numProjects!}
+        //             onUpdate={( index, limit ) => {
+        //                 if ( this.props.onProjectsRequested )
+        //                     this.props.onProjectsRequested( index, limit, this.state.searchText! )
+        //             } }>
+        //             <div className="project-items">
+        //                 <div className="error bad-input" style={{ display: ( this.state.errorMsg ? 'block' : '' ) }}>
+        //                     {this.state.errorMsg || ''}
+        //                 </div>
+        //                 {
+        //                     projects.map(( p ) => {
+        //                         return <ImagePreview key={p._id}
+        //                             className="project-item"
+        //                             selected={p.selected}
+        //                             src={p.image}
+        //                             label={p.name}
+        //                             labelIcon={<span className="fa fa-file" />}
+        //                             onDoubleClick={( e ) => {
+        //                                 this.selectProject( p, true );
+        //                                 e.stopPropagation();
+        //                             } }
+        //                             onClick={( e ) => {
+        //                                 this.selectProject( p, false );
+        //                                 e.stopPropagation();
+        //                             } }
+        //                             />
+        //                     })}
+        //                 <div className="no-items unselectable" style={{ display: ( projects.length ? 'none' : '' ) }}>
+        //                     {this.props.noProjectMessage}
+        //                 </div>
+        //             </div>
+        //         </Pager>
     }
 
     /*
@@ -47,84 +91,21 @@ export class ProjectList extends React.Component<IProjectListProps, IProjectList
      */
     selectProject( project: HatcheryServer.IProject | null, doubleClick: boolean ) {
 
-        if ( this.state.selectedProject && this.state.selectedProject !== project )
-            this.state.selectedProject.selected = false;
+        if ( this._selectedProject && this._selectedProject !== project )
+            this._selectedProject.selected = false;
 
         if ( !project ) {
-            this.setState( { selectedProject: null });
+            this._selectedProject = null;
             return;
         }
 
         project.selected = true;
-        this.setState( { selectedProject: project });
-        if ( this.props.onProjectSelected ) {
-            this.props.onProjectSelected( project );
+        this._selectedProject = project;
+        if ( this.onProjectSelected ) {
+            this.onProjectSelected( project );
 
-            if ( doubleClick && this.props.onProjectDClicked )
-                this.props.onProjectDClicked( project );
+            if ( doubleClick && this.onProjectDClicked )
+                this.onProjectDClicked( project );
         }
-    }
-
-    /**
-     * Creates the component elements
-     */
-    render(): JSX.Element {
-        const props: IProjectListProps = Object.assign( {}, this.props );
-        const projects = this.props.projects!;
-        delete props.noProjectMessage;
-        delete props.onProjectSelected;
-        delete props.onProjectDClicked;
-        delete props.projects;
-        delete props.numProjects;
-        delete props.onProjectsRequested;
-
-        return <div {...props} className={'project-list ' + ( props.className || '' )}>
-            <div className="projects-toolbar background">
-                {this.props.children}
-                <SearchBox placeholder="Keywords" onSearch={( e, text ) => {
-                    e; // Supresses unused param error
-                    this.state.searchText = text;
-                    ( this.refs[ 'pager' ] as Pager ).invalidate();
-                } } />
-                {this.state.loading ? <i className="fa fa-cog fa-spin fa-3x fa-fw"></i> : null}
-            </div>
-            <div className="projects-container" onClick={() => { this.selectProject( null, false ) } }>
-                <Pager
-                    limit={6}
-                    ref="pager"
-                    count={this.props.numProjects!}
-                    onUpdate={( index, limit ) => {
-                        if ( this.props.onProjectsRequested )
-                            this.props.onProjectsRequested( index, limit, this.state.searchText! )
-                    } }>
-                    <div className="project-items">
-                        <div className="error bad-input" style={{ display: ( this.state.errorMsg ? 'block' : '' ) }}>
-                            {this.state.errorMsg || ''}
-                        </div>
-                        {
-                            projects.map(( p ) => {
-                                return <ImagePreview key={p._id}
-                                    className="project-item"
-                                    selected={p.selected}
-                                    src={p.image}
-                                    label={p.name}
-                                    labelIcon={<span className="fa fa-file" />}
-                                    onDoubleClick={( e ) => {
-                                        this.selectProject( p, true );
-                                        e.stopPropagation();
-                                    } }
-                                    onClick={( e ) => {
-                                        this.selectProject( p, false );
-                                        e.stopPropagation();
-                                    } }
-                                    />
-                            })}
-                        <div className="no-items unselectable" style={{ display: ( projects.length ? 'none' : '' ) }}>
-                            {this.props.noProjectMessage}
-                        </div>
-                    </div>
-                </Pager>
-            </div>
-        </div>
     }
 }
