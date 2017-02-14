d810725121ae983c08c6d807ae525ae4eadff57f
Mathew Henson <mat@webinate.net>
2017-02-14 17:10:25 +0000

    Fixed user meta host

--- lib/models/user-meta.ts
+++ lib/models/user-meta.ts
@@ -1,12 +1,15 @@
 import { Model, IModelOptions } from './model';
 import { DB } from '../setup/db';
 
+/**
+ * Describes the user meta model
+ */
 export class UserMeta extends Model<HatcheryServer.IUserMeta> {
     constructor( options?: IModelOptions<HatcheryServer.IUserMeta> ) {
         super( Object.assign<IModelOptions<HatcheryServer.IUserMeta>>( options, {
             url: 'user-details/',
             idAttribute: 'user',
-            host: DB.USERS
+            host: DB.API
         }) );
     }
 }
